04656053cf036009c171bfca16a29d39cb73b050
Alumnos <Alumnos@192.168.3.150>
2024-03-11 10:18:30 -0300

    Agregado codigo

--- Clase_1/src/clase1/Anio.java
+++ Clase_1/src/clase1/Anio.java
@@ -3,8 +3,15 @@ package clase1;
 public class Anio {
 
 	public static void main(String[] args) {
-	
-
+	int a = 20241231;
+	int resul = a / 10000;
+	System.out.println(resul);
+	int resul2 = a % 100;
+	System.out.println(resul2);
+	int resul3 = a / 100;
+	System.out.println(resul3);
+	int si =resul3 % 100;
+	System.out.println(si);
 	}
 
 }
